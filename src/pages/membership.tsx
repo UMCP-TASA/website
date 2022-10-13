cd2feddb9d5f11dc2f79801d24feea667d1e838e
wyland1 <wyland@terpmail.umd.edu>
2022-10-12 20:21:20 -0400

    added box'd kitchen

--- src/pages/membership.tsx
+++ src/pages/membership.tsx
@@ -48,30 +48,29 @@ function MembershipPage(props: Props) {
             <PageContent>
                 <Section title="What is a TASA Membership Card?">
                     <Text align="center">
-                        A TASA membership card brings fun, happiness, 
-                        and premium benefits right to your door. These 
-                        benefits include 10% off Cuppa Tea, Class520, and Aroy 
-                        Thai (more to come!!). In addition, this card will award 
-                        you free event tickets AND raffle tickets at two of our 
-                        major events: Tour of Taiwan and Night Market! Receive all 
-                        of these perks for the small cost of $15, or get some 
+                        A TASA membership card brings fun, happiness,
+                        and premium benefits right to your door. These
+                        benefits include 10% off Aroy Thai, Box'd Kitchen, Class520, and Cuppa
+                        Tea (more to come!!). In addition, this card will award
+                        you free event tickets AND raffle tickets at two of our
+                        major events: Tour of Taiwan and Night Market! Receive all
+                        of these perks for the small cost of $15, or get some
                         friends together and buy a group bundle!
                     </Text>
                 </Section>
-                
+
                 <Section title="Current Benefits Include">
                     <ul>
-                        <li>
-                            <Text>10% off at <a href="https://www.cuppatea.us/">
-                                Cuppa Tea
-                            </a>
-                            </Text>
-                            
-                        </li>
                         <li>
                             <Text>10% off at <a href="https://www.aroythaicollegepark.com/">
                                 Aroy Thai
                             </a> - 10% off on any purchase $10 or more
+                            </Text>
+                        </li>
+                        <li>
+                            <Text>10% off at <a href="https://boxdkitchen.com/">
+                                Box'd Kitchen
+                            </a>
                             </Text>
                         </li>
                         <li>
@@ -81,12 +80,27 @@ function MembershipPage(props: Props) {
                             </Text>
                         </li>
                         <li>
+                            <Text>10% off at <a href="https://www.cuppatea.us/">
+                                Cuppa Tea
+                            </a>
+                            </Text>
+                        </li>
+                        <li>
                             <Text>Free Event Tickets to Tour of Taiwan and Night Market</Text>
                         </li>
                         <li>
                             <Text>Free Raffle Tickets at Tour of Taiwan and Night Market</Text>
                         </li>
                     </ul>
+                    <Button
+                        href="https://www.google.com/maps/d/u/0/edit?mid=1teAh6Njo7LKsmSuR9JUX8ZCwXEmU3Eg&usp=sharing"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        variant="contained"
+                        color="secondary"
+                    >
+                        Map of Restaurants
+                    </Button>
                 </Section>
 
                 <Section title="Pricing">
@@ -99,19 +113,19 @@ function MembershipPage(props: Props) {
                 <Section title="Form">
                     <Text>
                         Order Your Membership Card Today!
-                        </Text>
-                    <Button 
+                    </Text>
+                    <Button
                         href="https://docs.google.com/forms/d/e/1FAIpQLSfgdUDFC9smFFwF-4DvmULrPQSuNbf2nHQLZiskcDazZaW0WQ/viewform"
                         target="_blank"
                         rel="noreferrer noopener"
                         variant="contained"
                         color="secondary"
-                        >
-                            Order Now
+                    >
+                        Order Now
                     </Button>
                 </Section>
 
-                
+
             </PageContent>
 
         </>
