cbc960e0a6f8976b6a11ad4885daaeac65105633
wyland1 <wyland.terpmail.umd.edu>
2023-03-13 16:18:32 -0400

    for mem cards, nm uses one ticket (raffle/event)

--- src/pages/membership.tsx
+++ src/pages/membership.tsx
@@ -94,8 +94,7 @@ function MembershipPage(props: Props) {
                         </li>
                         <li>
                             <Text>
-                                Free Raffle Tickets at Tour of Taiwan and Night 
-                                Market
+                                Free Raffle Tickets at Tour of Taiwan
                             </Text>
                         </li>
                     </ul>
